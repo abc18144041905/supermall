bb577217e1bee42731a7c61ee85c8d367c35ceef
abc18144041905 <1129261789@qq.com>
2023-05-18 18:54:41 +0800

    提交

---
+++ src/views/categroy/childComps/TabMenu.vue
@@ -0,0 +1,69 @@
+<template>
+  <scroll id="tab-menu" :probeType = "3" :pullUpLoad="true">
+    <div class="menu-list">
+      <div class="menu-list-item"
+           :class="{active: index===currentIndex}"
+           v-for="(item, index) in Object.keys(categories)"
+           :key="index"
+           @click="itemClick(index)">
+        {{item}}
+      </div>
+    </div>
+  </scroll>
+</template>
+
+<script>
+  import Scroll from 'components/common/scroll/Scroll'
+
+	export default {
+		name: "TabMenu",
+    components: {
+		  Scroll
+    },
+    props: {
+		  categories:{
+        type:Object,
+        default(){
+          return {}
+        }
+      }
+    },
+    data() {
+		  return {
+		    currentIndex: 0
+      }
+    },
+    methods: {
+		  itemClick(index) {
+        this.currentIndex = index
+        this.$emit('selectItem', index)
+      }
+    },
+    computed:{
+      
+    }
+	}
+</script>
+
+<style scoped>
+  #tab-menu {
+    background-color: #f6f6f6;
+    height: 100%;
+    width: 100px;
+    box-sizing: border-box;
+  }
+
+  .menu-list-item {
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    font-size: 14px;
+  }
+
+  .menu-list-item.active {
+    font-weight: 700;
+    color: var(--color-high-text);
+    background-color: #fff;
+    border-left: 3px solid var(--color-high-text);
+  }
+</style>
---
+++ src/views/detial/childComps/DetialSwiper.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <swiper ref="swiper" v-if="childrentopImages.length">
+          <swiper-item v-for="(item, index) in childrentopImages" :key="index">
+              <img :src="item" alt="" @load="imageLoad">
+          </swiper-item>
+        </swiper>
+    </div>
+</template>
+
+<script>
+import {Swiper,SwiperItem} from 'components/common/swiper/index'
+export default {
+  data(){
+    return{
+      isLoad:false
+    }
+  },
+  props:{
+     childrentopImages:{
+		    type: Array,
+        default(){
+          return []
+        }
+      }
+  },
+  components:{
+     Swiper,
+     SwiperItem
+  },
+  methods: {
+		  stopTimer() {
+		    this.$refs.swiper.stopTimer()
+      },
+      startTimer() {
+		    if (this.$refs.swiper) {
+          this.$refs.swiper.startTimer()
+        }
+      },
+      imageLoad(){
+        if(!this.isLoad){
+           this.$emit('overimgload')
+           this.isLoad = true
+        }
+         
+      }
+    }
+}
+</script>
+
+<style>
+ 
+</style>
